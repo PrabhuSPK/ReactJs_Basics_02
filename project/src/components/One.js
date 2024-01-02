3a092af07833de4e9548c8f782e691b8b4d408dd
PrabhuSPK <prabhuselvamani1995@gmail.com>
2024-01-02 12:01:04 +0530

    Props-conditional rendering { bio &&  <Two bio={bio} />  }

--- project/src/components/One.js
+++ project/src/components/One.js
@@ -4,8 +4,8 @@ import Two from "./Two";
 
 function Name(){
   // const name="Prabhu"
-  // const bio = {"name":"prabhu","location":"musiri"}
-  const bio = {}
+  const bio = {"name":"prabhu","location":"musiri"}
+  // const bio = {}
   const data = {"age":29,"city":"bangalore"}
   const office = true //Office is open
   // const office = false //Office is closed
@@ -14,8 +14,10 @@ function Name(){
       <>
       <h1>i am one</h1>
       
-      { bio.name !== undefined && bio.location !== undefined ?
-       <Two bio={bio} /> : null }
+      {/* { bio.name !== undefined && bio.location !== undefined ?
+       <Two bio={bio} /> : null } */}
+
+       { bio &&  <Two bio={bio} />  }
      
       <Four data={data} />
       { office ? <p>Office is open</p> : <p>Office is closed</p>}
